14c4da5c3224d5a20c8323d5333a0fc33c7f925e
Ryan Roat <ryanroat@gmail.com>
2020-03-27 13:53:48 -0400

    index view of articles from db in rev chron

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 
 const app = express();
@@ -11,19 +12,24 @@ mongoose.connect('mongodb://localhost/blog', {
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false}));
 
-app.get('/', (req, res) => {
-    const articles = [
-        {
-            title: 'Test Article',
-            createdDate: new Date(),
-            description: 'test description'
-        },
-        {
-            title: 'Test Article 2',
-            createdDate: new Date(),
-            description: 'test description 2'
-        }
-    ];
+app.get('/', async (req, res) => {
+    // const articles = [
+    //     {
+    //         title: 'Test Article',
+    //         createdDate: new Date(),
+    //         description: 'test description'
+    //     },
+    //     {
+    //         title: 'Test Article 2',
+    //         createdDate: new Date(),
+    //         description: 'test description 2'
+    //     }
+    // ];
+
+    const articles = await Article.find().sort({
+        createdDate: 'desc'
+    });
+
     
     res.render('articles/index', { articles });
 });
